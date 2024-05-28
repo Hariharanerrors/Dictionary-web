3d539d36d29f9ad1b86ff391bfc43588e96a4294
Hariharanerrors <srdhari04@gmail.com>
2024-05-28 22:03:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
+import { FcSpeaker } from "react-icons/fc";
 
 function App() {
+
+  const [data, setData] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+
+  function getMeaning() {
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`)
+      .then((response) => {
+        setData(response.data[0]);
+      });
+  }
+  function playAudio() {
+    let audio = new Audio(data.phonetics[0].audio);
+    audio.play();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='free-dic'>
+        <h1>Free Dictionary</h1>
+        <div className='dic-search'>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => { setSearchWord(e.target.value); }}
+          />
+
+          <button
+            onClick={() => { getMeaning(); }}
+          >Search
+          </button>
+        </div>
+      </div>
+
+      {data && (
+        <div className='showresults'>
+          <h2>
+            {data.word}{" "}
+            <button
+              onClick={() => {
+                playAudio();
+              }}
+            >
+              <FcSpeaker size="26px" />
+            </button>
+          </h2>
+          <h4>Parts of speech:</h4>
+          <p>{data.meanings[0].partOfSpeech}</p>
+          <h4>Definition :</h4>
+          <p>{data.meanings[0].definitions[0].definition}</p>
+
+          <h4>Example:</h4>
+          <p>{data.meanings[0].definitions[0].example}</p>
+        </div>
+      )}
+
+    </>
+
   );
 }
 
